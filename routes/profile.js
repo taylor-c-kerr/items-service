4ed597d39161c9434a889c69f1931b6293cb6965
Taylor <taylor@Taylors-MacBook-Pro.local>
2018-01-20 13:17:43 -0500

    profile view

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,18 @@
+// I probably need to take this out beause I only want the express app to be the API
+// Logging in needs to be done on the client and the API will send responses depending on the role in the db
+// 
+// 
+
 var router = require('express').Router();
 
 var authCheck = (req, res, next) => {
-
+	if (!req.user) {
+		// user not lgged in.
+		res.redirect('/auth/login');
+	} else {
+		// user logged in. go on to the next part of middleware
+		next();
+	}
 }
 
 router.get('/', authCheck, (req, res) => {
