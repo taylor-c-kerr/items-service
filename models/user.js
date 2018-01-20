4b9f22037efb08a1fb93e39dcaa9f6c8852009cf
Taylor <taylor@Taylors-MacBook-Pro.local>
2018-01-20 14:31:57 -0500

    remove git ignore

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	_id : mongoose.Schema.Types.ObjectId,
+	googleId: String,
+	email: String
+});
+
+var User = mongoose.model('user', userSchema);
+
+module.exports = User;
