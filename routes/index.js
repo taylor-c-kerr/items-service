13e8d649b1a2da975740d25999d9622109c2178f
Taylor <taylor@Taylors-MacBook-Pro.local>
2018-01-20 14:30:12 -0500

    removed gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 var db = require('../queries')
 
+function isAuthenticated(req, res, next) {
+	if (req.user) {
+		return next():
+	} else {
+		res.send('Not authorized to see this!');
+	}
+}
 
-router.post('/api/items', db.postItem);
+
+router.post('/api/items', isAuthenticated, db.postItem);
 router.get('/api/items', db.getAllItems);
 router.get('/api/items/:id', db.getItem);
 router.patch('/api/items/:id', db.updateItem);
