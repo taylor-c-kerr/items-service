5121b1ab24a7bcbe55d61996c3a87ac2f116f94e
Taylor Kerr <tckerr14@gmail.com>
2019-03-05 16:16:02 -0500

    remove references to old collections

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const noun = require('../queries/nounQueries');
-const verb = require('../queries/verbQueries');
-const adjective = require('../queries/adjectiveQueries');
-const adverb = require('../queries/adverbQueries');
 const word = require('../queries/wordQueries');
-const name = require('../queries/nameQueries');
-
-
-router.post('/api/nouns', noun.postNoun); // curl -X POST http://localhost:8080/api/nouns -H 'Content-Type: application/json' -d '{"name":"pretzel","category":["food","test"]}'
-router.get('/api/nouns', noun.getAllNouns);
-router.get('/api/nouns/:id', noun.getNoun);
-router.patch('/api/nouns/:id', noun.updateNoun);
-router.delete('/api/nouns/:id', noun.deleteNoun);
-
-router.post('/api/verbs', verb.postVerb); // curl -X POST http://localhost:8080/api/verbs -H 'Content-Type: application/json' -d '{"name":"queef","category":["action","test"]}'
-router.get('/api/verbs', verb.getAllVerbs);
-router.get('/api/verbs/:id', verb.getVerb);
-router.patch('/api/verbs/:id', verb.updateVerb);
-router.delete('/api/verbs/:id', verb.deleteVerb);
-
-router.post('/api/adjectives', adjective.postAdjective); // curl -X POST http://localhost:8080/api/adjectives -H 'Content-Type: application/json' -d '{"name":"queef","category":["action","test"]}'
-router.get('/api/adjectives', adjective.getAllAdjectives);
-router.get('/api/adjectives/:id', adjective.getAdjective);
-router.patch('/api/adjectives/:id', adjective.updateAdjective);
-router.delete('/api/adjectives/:id', adjective.deleteAdjective);
-
-router.post('/api/adverbs', adverb.postAdverb); // curl -X POST http://localhost:8080/api/adverbs -H 'Content-Type: application/json' -d '{"name":"queef","category":["action","test"]}'
-router.get('/api/adverbs', adverb.getAllAdverbs);
-router.get('/api/adverbs/:id', adverb.getAdverb);
-router.patch('/api/adverbs/:id', adverb.updateAdverb);
-router.delete('/api/adverbs/:id', adverb.deleteAdverb);
-
-router.post('/api/names', name.postName); // curl -X POST http://localhost:8080/api/names -H 'Content-Type: application/json' -d '{"name":"queef","category":["action","test"]}'
-router.get('/api/names', name.getAllNames);
-router.get('/api/names/:id', name.getName);
-router.patch('/api/names/:id', name.updateName);
-router.delete('/api/names/:id', name.deleteName);
 
 router.post('/api/words', word.postWord); // curl -X POST http://localhost:8080/api/words -H 'Content-Type: application/json' -d '{"word":"queef","category":["action","test"]}'
 router.get('/api/words', word.getAllWords);
