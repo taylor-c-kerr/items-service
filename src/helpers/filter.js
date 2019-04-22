93db4a505873e825add5932c18236247bcf89add
Taylor Kerr <tckerr14@gmail.com>
2019-04-21 20:29:24 -0400

    linting

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -5,7 +5,7 @@ const error = new TypeError('Invalid query');
   ** @param {string} string a query string that is to be parsed
 */
 const equals = (string) => {
-  let criteria = {};
+  const criteria = {};
   if (string.indexOf('=') === -1) {
     return criteria;
   }
@@ -16,16 +16,16 @@ const equals = (string) => {
   string = string.split('=');
   criteria[string[0]] = new RegExp(`^${string[1]}$`, 'gi');
   return criteria;
-}
+};
 
 /*
   ** @param {string} string a query string that is to be parsed
 */
 const contains = (string) => {
-  let criteria = {};
+  const criteria = {};
 
   if (string.indexOf('~') === -1) {
-    return criteria
+    return criteria;
   }
   if (string.indexOf('\'') === -1) {
     throw error;
@@ -34,7 +34,7 @@ const contains = (string) => {
   string = string.split('~');
   criteria[string[0]] = new RegExp(`.*${string[1]}.*`, 'gi');
   return criteria;
-}
+};
 
 /*
   ** @param {string} string a query string that is to be parsed
@@ -42,33 +42,30 @@ const contains = (string) => {
 const determineQuery = (string) => {
   const query = string.indexOf('=') > -1 ? equals(string)
     : string.indexOf('~') > -1 ? contains(string)
-    : error;
+      : error;
 
-    return query;
-}
+  return query;
+};
 
 /*
   ** @param {string} filterString query string from the url that is parsed and then used to query the db
 */
 const filter = (filterString) => {
   filterString = querystring.unescape(filterString);
-  let query = {}
+  let query = {};
   const delimiter = /%20AND%20|%20OR%20| AND | OR /gi;
   const filters = filterString.split(delimiter);
   if (filters.length > 2) {
     throw error;
-  }
-  else if(filters.length === 1) {
-    query = determineQuery(filters[0])
-  }
-  else if (filterString.indexOf(' AND ') > -1) {
+  } else if (filters.length === 1) {
+    query = determineQuery(filters[0]);
+  } else if (filterString.indexOf(' AND ') > -1) {
     query['$and'] = [determineQuery(filters[0]), determineQuery(filters[1])];
-  }
-  else if (filterString.indexOf(' OR ') > -1) {
+  } else if (filterString.indexOf(' OR ') > -1) {
     query['$or'] = [determineQuery(filters[0]), determineQuery(filters[1])];
   }
 
   return query;
-}
+};
 
 module.exports = filter;
